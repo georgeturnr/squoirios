3a402f131ca11e53d7c7d5e4a91d546c17cbfe33
George Turner <gjetixturner@gmail.com>
2016-12-06 21:46:28 +0000

    Initial Commit

---
+++ Squoir/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Squoir
+//
+//  Created by George Turner on 06/12/2016.
+//  Copyright © 2016 George Turner. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
